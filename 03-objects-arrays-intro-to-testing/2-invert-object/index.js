60daff93274112382b57b01a0f6c1c59eebe2d4e
Pavel <kurilo.pavel@mail.ru>
2021-10-01 19:17:02 +0300

    Completed Invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,6 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  const rez = Object.fromEntries(new Array(Object.entries(obj).join().split(',').reverse()))
+  return rez
 }
